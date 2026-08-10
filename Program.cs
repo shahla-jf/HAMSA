0c6e7310211678909f517a11369d90d93bb779c8
shahla-jf <shahla.jafari.h@gmail.com>
2026-08-10 13:04:02 +0330

    add scoped IPaymentGateway

--- Program.cs
+++ Program.cs
@@ -155,6 +155,7 @@ builder.Services.AddScoped<IRevokedTokenRepository, RevokedTokenRepository>();
 builder.Services.AddScoped<LogoutHandler>();
 builder.Services.AddScoped<EditOwnerHandler>();
 builder.Services.AddScoped<EditTenantHandler>();
+builder.Services.AddScoped<IPaymentGateway, MockPaymentGateway>();
 builder.Services.AddScoped<SetMonthlyChargeAmountHandler>();
 builder.Services.AddScoped<GetChargeRatesHandler>();
 builder.Services.AddScoped<GetMyCurrentChargeHandler>();
