73440640be263db81b4e84680b43ae52ddb2b2c1
shahla-jf <shahla.jafari.h@gmail.com>
2026-08-11 16:29:33 +0330

    add payment status to shared costs

---
+++ Migrations/20260811125815_AddPaymentStatusToSharedCosts.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HAMSA.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentStatusToSharedCosts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCleaningPaid",
+                table: "Buildings",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsElectricityPaid",
+                table: "Buildings",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsElevatorPaid",
+                table: "Buildings",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsWaterPaid",
+                table: "Buildings",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCleaningPaid",
+                table: "Buildings");
+
+            migrationBuilder.DropColumn(
+                name: "IsElectricityPaid",
+                table: "Buildings");
+
+            migrationBuilder.DropColumn(
+                name: "IsElevatorPaid",
+                table: "Buildings");
+
+            migrationBuilder.DropColumn(
+                name: "IsWaterPaid",
+                table: "Buildings");
+        }
+    }
+}
