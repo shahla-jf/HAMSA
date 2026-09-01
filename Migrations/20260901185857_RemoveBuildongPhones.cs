1281510054789864eec3f99165d86b575ac651ae
shahla-jf <shahla.jafari.h@gmail.com>
2026-09-01 22:30:25 +0330

    remove building phone migration

---
+++ Migrations/20260901185857_RemoveBuildongPhones.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HAMSA.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveBuildongPhones : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FacilitiesPhone",
+                table: "Buildings");
+
+            migrationBuilder.DropColumn(
+                name: "LobbyPhone",
+                table: "Buildings");
+
+            migrationBuilder.DropColumn(
+                name: "ManagementPhone",
+                table: "Buildings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FacilitiesPhone",
+                table: "Buildings",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LobbyPhone",
+                table: "Buildings",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ManagementPhone",
+                table: "Buildings",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
